cdc9cdf596b38c897502595a419b4493c5b0d0ef
belorussky <vkp2010@yandex.ru>
2023-08-23 20:46:59 +0200

    Weather output

--- services/api.service.js
+++ services/api.service.js
@@ -1,8 +1,30 @@
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 import axios from 'axios';
 
+const getIcon = (icon) => {
+    switch (icon.slice(0, -1)) {
+		case '01':
+			return '☀️';
+		case '02':
+			return '🌤️';
+		case '03':
+			return '☁️';
+		case '04':
+			return '☁️';
+		case '09':
+			return '🌧️';
+		case '10':
+			return '🌦️';
+		case '11':
+			return '🌩️';
+		case '13':
+			return '❄️';
+		case '50':
+			return '🌫️';
+	}
+};
 
-const getWeather = async (lat, lon) => {
+const getWeatherByCoordinates = async (lat, lon) => {
     const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
     
     if (!token) {
@@ -19,8 +41,25 @@ const getWeather = async (lat, lon) => {
         }
     });
 
-    console.log(data);
     return data;
 };
 
-export { getWeather };
+const getWeather = async (city) => {
+    const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
+    
+    if (!token) {
+        throw new Error('API key not specified, set it via the command -t [API_KEY]');
+    }
+
+    const { data } = await axios.get('https://api.openweathermap.org/geo/1.0/direct', {
+        params: {
+            q: city,
+            appid: token
+        }
+    })
+    
+    return await getWeatherByCoordinates(data[0].lat,data[0].lon);
+
+};
+
+export { getWeather, getIcon };
--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
-import { getWeather } from './services/api.service.js';
-import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getWeather, getIcon } from './services/api.service.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
+import { saveKeyValue, getKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
     if (!token.length) {
@@ -17,19 +17,47 @@ const saveToken = async (token) => {
     }
 }
 
+const saveCity = async (city) => {
+    if (!city.length) {
+        printError('The city is not defined');
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city);
+        printSuccess('City is saved');
+    } catch (e) {
+        printError(e.message);
+    }
+}
+
+const getForcast = async () => {
+    try {
+        const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+        const weather = await getWeather(city);
+        printWeather(weather, getIcon(weather.weather[0].icon));
+    } catch (e) {
+        if (e?.response?.status == 404) {
+            printError('The city is specified incorrectly');
+        } else if (e?.response?.status == 401) {
+            printError('The token is specified incorrectly');
+        } else {
+            printError(e.message);
+        }
+    }
+};
+
 const initCLI = () => {
     const args = getArgs(process.argv);
     if (args.h) {
-        printHelp();
+        return printHelp();
     }
     if (args.s) {
-        //city
+        return saveCity(args.s);
     }
     if (args.t) {
         return saveToken(args.t);
     }
-    getWeather('50.06', '19.93');
-    //show weather
+    return getForcast();
 };
 
 initCLI();
